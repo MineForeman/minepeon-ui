a02796ea31c69e0290e83dfc6fc1fff0ee28810a
MineForeman <neil@MineForeman.com>
2014-08-04 00:02:20 +0000

    Write current hashrate to settings

--- http/ajax/hashrate.php
+++ http/ajax/hashrate.php
@@ -4,9 +4,18 @@
 include('settings.inc.php');
 require('miner.inc.php');
 
+// Save the hashrate to minepeon.conf
+
+
 $return = miner("summary", "");
 
 $hashrate = $return['SUMMARY'][0]['MHSav'] * 1000;
+
+// Save the hashrate to minepeon.conf
+
+$settings['current_hashrate'] = $hashrate;
+
+writeSettings($settings);
 
 $hashrate = time() . ':' . $hashrate;
 
